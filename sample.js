39dfcd28afcbde94d035109de14f34371adaa9d9
n-haruhi <hr23nakanishi@gmail.com>
2023-12-11 13:24:01 +0000

    js変数宣言

---
+++ sample.js
@@ -0,0 +1,18 @@
+// varによる再宣言
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
+
+// var以外は再宣言できないのでエラーになる。
+// letによる再宣言
+let nickname = "taro"
+console.log(nickname)
+let nickname = "ichiro"
+console.log(nickname)
+
+// constによる再宣言
+const nickname = "taro"
+console.log(nickname)
+const nickname = "ichiro"
+console.log(nickname)
